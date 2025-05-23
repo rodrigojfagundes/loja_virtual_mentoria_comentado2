a4e138c663578f787c0029a7636bdffd5a6dc2e9
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-23 20:45:58 -0300

    Configuracoes de performances e teste de CRUD

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -3,9 +3,19 @@ package jdev.mentoria.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+//@EntityScan mapeia as class model e gera as tabela no banco
+//@ComponentScan busca em todo o projeto anotacoes do SpringBoot
+//@EnableJPARepositories para indicar qual a pasta de repository
+//@EnableTransactionManagement para gerenciar as transacoes com o banco
 @SpringBootApplication
 @EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.mentoria.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
