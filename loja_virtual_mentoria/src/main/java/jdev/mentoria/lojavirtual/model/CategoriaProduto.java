20d49b70ad1a2f3df79d6d15e5d9a0f7ee376c45
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-23 21:51:38 -0300

    CRUD de categoria de produtos

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -36,19 +36,19 @@ public class CategoriaProduto implements Serializable{
 
 	
 	//MTAS CATEGORIAPRODUTO para 1 EMPRESA 
-	//(e EMPRESA e uma PESSOA do tipo juridica)
+	//(e EMPRESA e uma PESSOAJURIDICA)
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "empresa_id", nullable = false,
 	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, 
 	name = "empresa_id_fk"))
-	private Pessoa empresa;
+	private PessoaJuridica empresa = new PessoaJuridica();
 
 
-	public Pessoa getEmpresa() {
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
 	}
 
