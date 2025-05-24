a3e7fdc9b0758c2d9af0195c121c5fadacbb1e14
Rodrigo <Rodrigo@DESKTOP-NG2MGDC>
2025-05-24 11:22:56 -0300

    SandBox e ambiente de prod

---
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/enums/ApiTokenIntegracao.java
@@ -0,0 +1,11 @@
+package jdev.mentoria.lojavirtual.enums;
+
+public class ApiTokenIntegracao {
+	
+	
+	
+	public static final String TOKEN_MELHOR_ENVIO_SAND_BOX = "Token_Melhor_Envio";
+	public static final String URL_MELHOR_ENVIO_SAND_BOX = "https://sandbox.melhorenvio.com.br/";
+	
+	
+}
